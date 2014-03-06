35bc47fd0e877adb6b6ed3ba981b117d9f657e3c
Durka Durka <battlefielder21@yahoo.co.uk>
2014-03-06 05:21:59 -0500

    dig lines 6 and 7 and 8 and 9

--- comments.py
+++ comments.py
@@ -2,3 +2,10 @@
 print("This is not a comment.")
 #This is a comment
 #This is also a comment.It won't print("anything")
+print("This is not a comment.")#although this is...
+"""
+These
+are
+all
+comments
+"""
